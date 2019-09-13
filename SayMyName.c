0ece3afe6800b0511f2ce59768db17a6eee5b0c7
satkins14 <55179217+satkins14@users.noreply.github.com>
2019-09-13 07:24:18 -0500

    Create SayMyName.c

---
+++ SayMyName.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Samuel Atkins
+ * Date: 2019/9/13
+ *
+ * Says my name
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Samuel Atkins\n");
+
+  return 0;
+}
